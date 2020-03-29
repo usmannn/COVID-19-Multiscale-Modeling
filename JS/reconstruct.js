2b68fc3d581ef1e6f84d05bfcdbd9cb8692860fc
usmannn <usmannn@users.noreply.github.com>
2020-03-29 16:02:00 -0400

    source file to run prediction model

---
+++ JS/reconstruct.js
@@ -0,0 +1,17 @@
+// reconstructs the network
+// reruns the prediction model on updated network
+// updates the feature layer (data source) with new covid-19 spread prediction data
+ 
+function reconstruct()
+{
+	alert("Reconstruct the Network: To be implemented...");
+	
+	
+	// In this feature layer, the new prediction data needs to be updated
+	/*
+	var layer = new FeatureLayer("https://services.arcgis.com/4TKcmj8FHh5Vtobt/arcgis/rest/services/Dummy_COVID19_Spread_Temporal_Data/FeatureServer/0",
+	{
+		outFields: [ "*" ]
+	});
+  */
+}
