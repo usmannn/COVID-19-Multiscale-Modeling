cb3e71475b16ec2a6e133ea18bb716edaf8a9f0e
usmannn <usmannn@users.noreply.github.com>
2020-03-29 18:25:54 -0400

    making selection feature to work

--- JS/site.js
+++ JS/site.js
@@ -17,12 +17,43 @@ require([
 	"esri/widgets/Feature"
 ], function(WebMap, MapView, LayerList, TimeSlider, Expand, Collection, Legend, FeatureLayer, Popup, Feature) {
 
+// popup configuration
+var popupTemplate = {
+          title: "Country: {Country_name}",
+          actions: [
+			  {
+                title: "Remove from Predictions",
+				id: "removeFromPrediction"
+              }
+			],
+		  content: [
+              {
+                type: "fields",
+                fieldInfos: [                  
+                  {
+                    fieldName: "Active",
+                    label: "Active Cases"
+                  },
+                  {
+                    fieldName: "Death",
+                    label: "Total Deaths"
+                  },
+                  {
+                    fieldName: "Recover",
+                    label: "Total Recovered"
+                  }
+                ]
+			  }
+			]
+        };
+		
 //const layer = webmap.findLayerById('40b129da4bd84efa9993b768b8c6ead6');		
 var layer = new FeatureLayer("https://services.arcgis.com/4TKcmj8FHh5Vtobt/arcgis/rest/services/Dummy_COVID19_Spread_Temporal_Data/FeatureServer/0",
 	{
 		outFields: [ "*" ],
 		useViewTime: false,
-		popupEnabled: true
+		popupEnabled: true,
+		popupTemplate: popupTemplate
 	}
 );
 
@@ -38,7 +69,7 @@ var view = new MapView({
   map: webmap
 });
 
-view.popup.defaultPopupTemplateEnabled  = true;
+//view.popup.defaultPopupTemplateEnabled  = true;
 
 var layerList = new LayerList({
   view: view
@@ -66,41 +97,55 @@ view.ui.add("titleDiv", "top-right");
 
 // accessing layer with temporal data from the webmap
 		
-view.when().then(function() {
+view.whenLayerView(layer).then(function(lv) {
+  const fullTimeExtent = layer.timeInfo.fullTimeExtent;
+  
+  
+  // set up time slider properties
+  timeSlider.fullTimeExtent = fullTimeExtent;
+  timeSlider.stops = {
+	interval: layer.timeInfo.interval
+  };
+  
+  // why this is null ?? Slider's buttons are disabled for some reason
+  console.log(layer.timeInfo.interval);
+});
 
-	// Create a default graphic for when the application starts
-	const graphic = {
-		popupTemplate: {
-		  content: "Mouse over features to show details..."
+// Adding selected feature to restricted country list when removeFromPrediction is clicked 
+view.when(function() {
+	var popup = view.popup;
+	popup.viewModel.on("trigger-action", function(event) {
+		if (event.action.id === "removeFromPrediction") {
+						
+			var attributes = popup.viewModel.selectedFeature.attributes;
+			//console.log(attributes);
+			//alert("Action called: removeFromPrediction() | object id: " + attributes.ObjectId);
+			
+			// if not already exist, add the id to selected_ids list
+			if(!selected_ids.includes(attributes.ObjectId))
+			{
+				selected_ids.push(attributes.ObjectId);
+				
+				// make an entry for the selected feature in restricted countries panel
+				var table = document.getElementById("table_restricted_countries");
+				var row = table.insertRow(-1);
+				
+				var cell_id = row.insertCell(-1);
+				cell_id.innerHTML = attributes.ObjectId;
+				cell_id.style.visibility = 'hidden';
+				
+				var cell_name = row.insertCell(-1);
+				cell_name.innerHTML = attributes.Country_name;
+				cell_name.style.fontWeight = "bold";
+				
+				var cell_action = row.insertCell(-1);
+				cell_action.innerHTML = "<button type=\"button\" class=\"btn btn-primary\" onclick=\"clearCountrySelection(this)\"> Clear </button>";
+			}
 		}
-	};
-	
-	// Provide graphic to a new instance of a Feature widget
-	/*
-	const feature = new Feature({
-		container: "selected-features",
-		graphic: graphic,
-		map: view.map,
-		spatialReference: view.spatialReference
-	});
-	*/
-
-	view.whenLayerView(layer).then(function(lv) {
-	  const fullTimeExtent = layer.timeInfo.fullTimeExtent;
-	  
-	  
-	  // set up time slider properties
-	  timeSlider.fullTimeExtent = fullTimeExtent;
-	  timeSlider.stops = {
-		interval: layer.timeInfo.interval
-	  };
-	  
-	  // why this is null ?? Slider's buttons are disabled for some reason
-	  console.log(layer.timeInfo.interval);
 	});
 });
 
-var button_div = document.getElementById("construct_button");
-view.ui.add(button_div, "top-right");
+var button_reconstruct = document.getElementById("construct_button");
+view.ui.add(button_reconstruct, "top-right");
 
 });
