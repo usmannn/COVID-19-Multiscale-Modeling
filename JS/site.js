711ccb879b3b2a465984166940852caa590e0d97
usmannn <usmannn@users.noreply.github.com>
2020-04-01 15:10:08 -0400

    Update site.js

--- JS/site.js
+++ JS/site.js
@@ -76,175 +76,109 @@ var view = new MapView({
 //view.popup.defaultPopupTemplateEnabled  = true;
 
 view.when(function() {
-	console.log("map loaded...");
-	// here we use Papa Parse to load and read the CSV data
-	// we could have also used another library like D3js to do the same
-	Papa.parse('Data/flight_routes_processed_v2_flowmap_reduced.csv', {
-		download: true,
-		header: true,
-		dynamicTyping: true,
-		skipEmptyLines: true,
-		complete: handleCsvParsingComplete
-		/*
-		worker: true,
-		step: function(row) {
-			console.log("Row: " + row.data);
-		},
-		complete: dataLoaded
-		*/
-	});
-	console.log("parsing data...");
-});
+    // here we use Papa Parse to load and read the CSV data
+    // we could have also used another library like D3js to do the same
+    Papa.parse('Data/flight_routes_processed_v2_flowmap_reduced.csv', {
+      download: true,
+      header: true,
+      dynamicTyping: true,
+      skipEmptyLines: true,
+      complete: handleCsvParsingComplete
+    });
+  });
 
-let canvasFlowmapLayer;
-function handleCsvParsingComplete(results) {
-	console.log("inside handleCsvParsingComplete...");
-	var graphicsFromCsvRows = results.data.map(function(datum) {
-		return new Graphic({
-			geometry: {
-				type: 'point',
-				longitude: datum.From_Longitude,
-				latitude: datum.From_Latitude
-			},
-			attributes: datum,
-			symbol: {
-				type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
-				color: [226, 119, 40],
-				outline: {
-					// autocasts as new SimpleLineSymbol()
-					color: [255, 255, 255],
-					width: 2
-				}
-			}
-		});
-	});
-	
-	console.log("done creating graphics for csv rows...");
+  function handleCsvParsingComplete(results) {
+    var graphicsFromCsvRows = results.data.map(function(datum) {
+      return new Graphic({
+        geometry: {
+          type: 'point',
+          longitude: datum.From_Longitude,
+	  latitude: datum.From_Latitude
+        },
+        attributes: datum
+      });
+    });
 
-	console.log(graphicsFromCsvRows.length);
-	console.log(graphicsFromCsvRows);
+    var canvasFlowmapLayer = new CanvasFlowmapLayer({
+      // array of Graphics
+      graphics: graphicsFromCsvRows,
 
-	canvasFlowmapLayer = new CanvasFlowmapLayer({
-		// array of Graphics
-		graphics: graphicsFromCsvRows,
-
-		// information about the uniqe origin-destinatino fields and geometries
-		originAndDestinationFieldIds: {
-			originUniqueIdField: 'From_Airport_Code',
-			originGeometry: {
-				x: 'From_Longitude',
-				y: 'From_Latitude',
-				spatialReference: {
-				wkid: 4326
-			}
-			},
-			destinationUniqueIdField: 'To_Airport_Code',
-			destinationGeometry: {
-				x: 'To_Longitude',
-				y: 'To_Latitude',
-				spatialReference: {
-				wkid: 4326
-				}
-			}
-		}
-	});
-
-	view.map.layers.add(canvasFlowmapLayer);
-
-	var layerList = new LayerList({
-		view: view
-	});
-
-	const layersExpand = new Expand({
-		expandIconClass: "esri-icon-collection",
-		expandTooltip: "Layers",
-		view: view,
-		content: layerList,
-		expanded: false
-	});
-	view.ui.add(layersExpand, "top-left");
-	
-	view.whenLayerView(canvasFlowmapLayer).then(function(canvasFlowmapLayerView) {
-	      // automatically select a few ORIGIN locations for path display
-	      // in order to demonstrate the flowmap functionality,
-	      // without being overwhelming and showing all O-D relationships
-
-	      // Hamid Karzai International Airport,Kabul,Afghanistan
-	      canvasFlowmapLayerView.selectGraphicsForPathDisplayById('From_Airport_Code', 7500, true, 'SELECTION_NEW');		
-	});
-}	
-/*
-// time slider widget initialization
-const timeSlider = new TimeSlider({
-  container: "timeSlider",
-  //mode: "time-window",
-  mode: "instant",
-  view: view
-});
-view.ui.add(timeSlider, "manual");
-*/
-
-// add the UI for titles, stats and chart.
-view.ui.add("titleDiv", "top-right");
-
-/*
-// accessing layer with temporal data from the webmap
-let timeLayerView;		
-view.whenLayerView(layer).then(function(lv) {
-  timeLayerView = lv;
-  const fullTimeExtent = layer.timeInfo.fullTimeExtent;
-  // set up time slider properties
-  timeSlider.fullTimeExtent = fullTimeExtent;
-  timeSlider.stops = {
-	interval: {
-		value: 1,
-		unit: "days"
+      // information about the uniqe origin-destinatino fields and geometries
+      originAndDestinationFieldIds: {
+        originUniqueIdField: 'From_Airport_Code',
+	originGeometry: {
+		x: 'From_Longitude',
+		y: 'From_Latitude',
+		spatialReference: {
+		wkid: 4326
 	}
-  };
-});
-
-timeSlider.watch("timeExtent", function(value){
-  timeLayerView.filter = {
-    timeExtent: value
-  };
-});
-*/
-
-// Adding selected feature to restricted country list when removeFromPrediction is clicked 
-view.when(function() {
-	var popup = view.popup;
-	popup.viewModel.on("trigger-action", function(event) {
-		if (event.action.id === "removeFromPrediction") {
-						
-			var attributes = popup.viewModel.selectedFeature.attributes;
-			//console.log(attributes);
-			//alert("Action called: removeFromPrediction() | object id: " + attributes.ObjectId);
-			
-			// if not already exist, add the id to selected_ids list
-			if(!selected_ids.includes(attributes.ObjectId))
-			{
-				selected_ids.push(attributes.ObjectId);
-				
-				// make an entry for the selected feature in restricted countries panel
-				var table = document.getElementById("table_restricted_countries");
-				var row = table.insertRow(-1);
-				
-				var cell_id = row.insertCell(-1);
-				cell_id.innerHTML = attributes.ObjectId;
-				cell_id.style.visibility = 'hidden';
-				
-				var cell_name = row.insertCell(-1);
-				cell_name.innerHTML = attributes.Country_name;
-				cell_name.style.fontWeight = "bold";
-				
-				var cell_action = row.insertCell(-1);
-				cell_action.innerHTML = "<button type=\"button\" class=\"btn btn-primary\" onclick=\"clearCountrySelection(this)\"> Clear </button>";
-			}
+	},
+	destinationUniqueIdField: 'To_Airport_Code',
+	destinationGeometry: {
+		x: 'To_Longitude',
+		y: 'To_Latitude',
+		spatialReference: {
+		wkid: 4326
 		}
-	});
-});
+	}
+      }
+    });
 
-var button_reconstruct = document.getElementById("construct_button");
-view.ui.add(button_reconstruct, "top-right");
+    view.map.layers.add(canvasFlowmapLayer);
 
+    // get access to the CanvasFlowmapLayer's layerView to make modifications
+    // of which O-D relationships are flagged for path display
+    view.whenLayerView(canvasFlowmapLayer).then(function(canvasFlowmapLayerView) {
+      // automatically select a few ORIGIN locations for path display
+      // in order to demonstrate the flowmap functionality,
+      // without being overwhelming and showing all O-D relationships
+
+      // Reykjavík
+      canvasFlowmapLayerView.selectGraphicsForPathDisplayById('From_Airport_Code', 5539, true, 'SELECTION_NEW');
+
+      // Alexandria
+      canvasFlowmapLayerView.selectGraphicsForPathDisplayById('From_Airport_Code', 7500, true, 'SELECTION_ADD');
+
+      // Tokyo
+      canvasFlowmapLayerView.selectGraphicsForPathDisplayById('From_Airport_Code', 13755, true, 'SELECTION_ADD');
+
+      // establish a hitTest to try to select new O/D relationships
+      // for path display from user interaction;
+      // try either 'pointer-move' or 'click' to see the effects
+      view.on('pointer-move', function(event) {
+      // view.on('click', function(event) {
+        var screenPoint = {
+          x: event.x,
+          y: event.y
+        };
+        view.hitTest(screenPoint).then(function(response) {
+          if (!response.results.length) {
+            return;
+          }
+
+          // check if the graphic(s) belongs to the layer of interest
+          // and mark them as selected for Bezier path display
+          response.results.forEach(function(result) {
+            if (result.graphic.layer === canvasFlowmapLayer) {
+              if (result.graphic.isOrigin) {
+                canvasFlowmapLayerView.selectGraphicsForPathDisplayById(
+                  'From_Airport_Code',
+                  result.graphic.attributes.From_Airport_Code,
+                  result.graphic.attributes.isOrigin,
+                  'SELECTION_NEW'
+                );
+              } else {
+                canvasFlowmapLayerView.selectGraphicsForPathDisplayById(
+                  'To_Airport_Code',
+                  result.graphic.attributes.To_Airport_Code,
+                  result.graphic.attributes.isOrigin,
+                  'SELECTION_NEW'
+                );
+              }
+            }
+          });
+        });
+      });
+    });
+  }
 });
