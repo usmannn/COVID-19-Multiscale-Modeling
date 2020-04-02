8bed823fb096d3d32539e530bf499583e09976e4
usmannn <usmannn@users.noreply.github.com>
2020-04-02 01:53:47 -0400

    Update site.js

--- JS/site.js
+++ JS/site.js
@@ -113,7 +113,58 @@ timeSlider.watch("timeExtent", function(value){
   };
 });
 
-  view.when(function() {
+var layerList = new LayerList({
+    view: view
+  });		
+
+  const layersExpand = new Expand({
+    expandIconClass: "esri-icon-collection",
+    expandTooltip: "Layers",
+    view: view,
+    content: layerList,
+    expanded: false
+  });
+
+  // Adding selected feature to restricted country list when removeFromPrediction is clicked 
+view.when(function() {
+	var popup = view.popup;
+	popup.viewModel.on("trigger-action", function(event) {
+		if (event.action.id === "removeFromPrediction") {
+						
+			var attributes = popup.viewModel.selectedFeature.attributes;
+			//console.log(attributes);
+			//alert("Action called: removeFromPrediction() | object id: " + attributes.ObjectId);
+			
+			// if not already exist, add the id to selected_ids list
+			if(!selected_ids.includes(attributes.ObjectId))
+			{
+				selected_ids.push(attributes.ObjectId);
+				
+				// make an entry for the selected feature in restricted countries panel
+				var table = document.getElementById("table_restricted_countries");
+				var row = table.insertRow(-1);
+				
+				var cell_id = row.insertCell(-1);
+				cell_id.innerHTML = attributes.ObjectId;
+				cell_id.style.visibility = 'hidden';
+				
+				var cell_name = row.insertCell(-1);
+				cell_name.innerHTML = attributes.Country_name;
+				cell_name.style.fontWeight = "bold";
+				
+				var cell_action = row.insertCell(-1);
+				cell_action.innerHTML = "<button type=\"button\" class=\"btn btn-primary\" onclick=\"clearCountrySelection(this)\"> Clear </button>";
+			}
+		}
+	});
+});
+	
+view.ui.add(layersExpand, "top-left");
+view.ui.add("titleDiv", "top-right");
+var button_reconstruct = document.getElementById("construct_button");
+view.ui.add(button_reconstruct, "top-right");
+ 
+view.when(function() {
     // here we use Papa Parse to load and read the CSV data
     // we could have also used another library like D3js to do the same
     Papa.parse('Data/flight_routes_processed_v2_flowmap_reduced.csv', {
@@ -209,57 +260,4 @@ timeSlider.watch("timeExtent", function(value){
       });
     });
   }
-
-  var layerList = new LayerList({
-    view: view
-  });		
-
-  const layersExpand = new Expand({
-    expandIconClass: "esri-icon-collection",
-    expandTooltip: "Layers",
-    view: view,
-    content: layerList,
-    expanded: false
-  });
-
-  // Adding selected feature to restricted country list when removeFromPrediction is clicked 
-view.when(function() {
-	var popup = view.popup;
-	popup.viewModel.on("trigger-action", function(event) {
-		if (event.action.id === "removeFromPrediction") {
-						
-			var attributes = popup.viewModel.selectedFeature.attributes;
-			//console.log(attributes);
-			//alert("Action called: removeFromPrediction() | object id: " + attributes.ObjectId);
-			
-			// if not already exist, add the id to selected_ids list
-			if(!selected_ids.includes(attributes.ObjectId))
-			{
-				selected_ids.push(attributes.ObjectId);
-				
-				// make an entry for the selected feature in restricted countries panel
-				var table = document.getElementById("table_restricted_countries");
-				var row = table.insertRow(-1);
-				
-				var cell_id = row.insertCell(-1);
-				cell_id.innerHTML = attributes.ObjectId;
-				cell_id.style.visibility = 'hidden';
-				
-				var cell_name = row.insertCell(-1);
-				cell_name.innerHTML = attributes.Country_name;
-				cell_name.style.fontWeight = "bold";
-				
-				var cell_action = row.insertCell(-1);
-				cell_action.innerHTML = "<button type=\"button\" class=\"btn btn-primary\" onclick=\"clearCountrySelection(this)\"> Clear </button>";
-			}
-		}
-	});
-});
-
-  view.ui.add(layersExpand, "top-left");
-  view.ui.add("titleDiv", "top-right");
-  var button_reconstruct = document.getElementById("construct_button");
-  view.ui.add(button_reconstruct, "top-right");
-  //console.log(timeSlider);
-  //view.ui.add(timeSlider, "manual");
 });
