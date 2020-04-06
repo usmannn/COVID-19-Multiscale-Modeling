f81304b89c4ff1bb0ab92da0f8553c69a0022e80
usmannn <usmannn@users.noreply.github.com>
2020-04-05 20:44:33 -0400

    Update reconstruct.js

--- JS/reconstruct.js
+++ JS/reconstruct.js
@@ -2,9 +2,30 @@
 // reruns the prediction model on updated network
 // updates the feature layer (data source) with new covid-19 spread prediction data
  
+function addMarkerOnSlider()
+{
+	var marker = "|";
+	var sliderTrack = document.getElementsByClassName("esri-slider__track");
+	var childs = sliderTrack.childNodes;
+	var lastChild = childs[childs.length];
+	
+	var lastChildScale = lastChild.style.transform.split(" ");
+	
+	var markerDiv = document.createElement("div");
+	markerDiv.style.color = "red";
+	markerDiv.style.fontSize = "24px";
+	markerDiv.style.transform = lastChildScale[2]+lastChildScale[3];
+	markerDiv.innerHTML = marker;
+	
+	childs[1].appendChild(markerDiv);
+	
+}
+
 function reconstruct(restricted_list)
 {
 	alert("Reconstruct the Network (not implemented)\nExcluded Countries: " + restricted_list[0] + "\nExcluded Connections: " + restricted_list[1]);
+	
+	addMarkerOnSlider();
 	
 	
 	// In this feature layer, the new prediction data needs to be updated
