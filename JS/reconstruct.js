f0bd99e55b9e1aa8d1fb3883ad616f6658c26137
usmannn <usmannn@users.noreply.github.com>
2020-04-06 01:58:38 -0400

    adding markers on the slider

--- JS/reconstruct.js
+++ JS/reconstruct.js
@@ -1,7 +1,26 @@
 // reconstructs the network
 // reruns the prediction model on updated network
 // updates the feature layer (data source) with new covid-19 spread prediction data
- 
+
+function addMarkerOnSlider()
+{
+	var marker = "|";
+	
+	var scaleElement = document.getElementsByClassName("esri-slider__segment esri-slider__segment-1");		
+	var eleTransform = scaleElement[0].style.transform;
+	
+	var scaleList = eleTransform.split(" ");
+	var scale = scaleList[0]+scaleList[1];
+	
+	var markerDiv = document.createElement("div");
+	markerDiv.style.color = "red";
+	markerDiv.style.fontSize = "32px";
+	markerDiv.style.transform = scale;
+	markerDiv.innerHTML = marker;
+	
+	scaleElement[0].parentNode.insertBefore(markerDiv, scaleElement[0].nextSibling);
+}
+
 function reconstruct(restricted_list)
 {
 	alert("Reconstruct the Network (not implemented)\nExcluded Countries: " + restricted_list);
