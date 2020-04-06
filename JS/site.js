312db7ee45139d4c82a07d085be62d13d18feecc
usmannn <usmannn@users.noreply.github.com>
2020-04-06 13:50:22 -0400

    Update site.js

--- JS/site.js
+++ JS/site.js
@@ -42,6 +42,8 @@ require([
   'esri/geometry/geometryEngine',
   'esri/symbols/SimpleLineSymbol',
   'esri/layers/GraphicsLayer',
+  'esri/core/sql/WhereClause',
+  'esri/intl',
   'dojo/domReady!'
 ], function(
   CanvasFlowmapLayer,
@@ -58,7 +60,9 @@ require([
   Draw,
   geometryEngine,
   SimpleLineSymbol,
-  GraphicsLayer
+  GraphicsLayer,
+  WhereClause,
+  intl
 ) {
     
 // popup configuration
@@ -75,25 +79,21 @@ popupTemplate = {
       type: "fields",
       fieldInfos: [                  
 	  {
-	    fieldName: "susceptible",
+	    fieldName: "s",
 	    label: "Susceptible"
 	  },
 	  {
-	    fieldName: "Death",
+	    fieldName: "d",
 	    label: "Total Deaths"
 	  },
 	  {
-	    fieldName: "infectious",
+	    fieldName: "i",
 	    label: "Infectious"
 	  },
 	  {
-	    fieldName: "recovered",
+	    fieldName: "r",
 	    label: "Recovered"
-	  },
-	  {
-	    fieldName: "dead",
-	    label: "Deaths"
-	  },
+	  },	  
 	  {
 	    fieldName: "population",
 	    label: "Total Population"
@@ -104,7 +104,7 @@ popupTemplate = {
 };
 		
   //const layer = webmap.findLayerById('40b129da4bd84efa9993b768b8c6ead6');		
-  layer = new FeatureLayer("https://services.arcgis.com/4TKcmj8FHh5Vtobt/arcgis/rest/services/COVID_19_Spread/FeatureServer/0",
+  layer = new FeatureLayer("https://services.arcgis.com/4TKcmj8FHh5Vtobt/arcgis/rest/services/COVID_19_Spread_v2/FeatureServer/0",
   //layer = new FeatureLayer("https://services.arcgis.com/4TKcmj8FHh5Vtobt/arcgis/rest/services/Dummy_COVID19_Spread_Temporal_Data/FeatureServer/0",
     {
       outFields: [ "*" ],
@@ -145,7 +145,7 @@ view.ui.add(timeSlider, "manual");
 view.ui.add("titleDiv", "top-right");
 
 // accessing layer with temporal data from the webmap
-let timeLayerView;		
+let timeLayerView, currentTimeExtent;
 view.whenLayerView(layer).then(function(lv) {
   timeLayerView = lv;
   const fullTimeExtent = layer.timeInfo.fullTimeExtent;
@@ -159,10 +159,17 @@ view.whenLayerView(layer).then(function(lv) {
   };
 });
 
-timeSlider.watch("timeExtent", function(value){
-  timeLayerView.filter = {
-    timeExtent: value
-  };
+timeSlider.watch("timeExtent", function(timeExtent){
+	var _d = new Date(timeExtent.start);
+	var formattedTimeExtent = intl.formatDate(_d .setDate(_d .getDate() + 1), dateFormatIntlOptions);
+	currentTimeExtent = formattedTimeExtent;
+	
+	if (view.map.findLayerById("connections")) {
+		if(view.map.findLayerById("connections").graphics.length > 0)
+		{
+		      view.map.findLayerById("connections").graphics.removeAll();
+		}
+	}
 });
 
 var layerList = new LayerList({
@@ -230,33 +237,88 @@ view.whenLayerView(layer).then(function(layerView) {
 		x: event.x,
 		y: event.y
 	   };
-	   console.log("clicked");
+	  
 	   view.hitTest(screenPoint).then(function(response) {
 		if (!response.results.length) {
+			var edgesDiv = document.getElementById("edgesDiv");
+			edgesDiv.innerHTML = "";
+
+			// remove previous selection
+			if(view.map.findLayerById("connections").graphics.length > 0)
+			{
+				view.map.findLayerById("connections").graphics.removeAll();
+			}
 			return;
 		}
-	   console.log("hittest clicked");
-	   console.log(response.results);
-		// check if the graphic(s) belongs to the layer of interest
-		// and mark them as selected for Bezier path display
-		response.results.forEach(function(result) {
-			console.log(result);
-			console.log(result.graphic.layer);
-			console.log(layer);
-			if (result.graphic.layer === layer) {
-				//alert("FeatureLayer object selected...");
+		for (p=0; p < response.results.length; p++)
+		{
+			var res = response.results[p];
+			if (res.graphic.layer === layer)
+			{
+				var query = layer.createQuery();
+				query.where = "Date = date'"+currentTimeExtent+"'";
+				layer.queryFeatures(query)
+				  .then(function(response){
+					// Create a symbol for drawing the line
+					var lineSymbol = {
+					  type: "simple-line", // autocasts as SimpleLineSymbol()
+					  color: [255,0,0,0.5],
+					  width: 0.75,
+					  cap : "round"
+					};
 				
-				const query = layerView.layer.createQuery();
-				query.where = "ID <> '" + result.graphic.attributes.ID + "'";
-				query.outFields = ["*"];
-				console.log(query);
-				layerView.queryFeatures(query)
-			        .then(function(response){
-			          console.log(response);
-			        });
+					var edgesDiv = document.getElementById("edgesDiv");
+					var _html = "<table id=\"edge_list_table\" class=\"table table-dark\" style=\"color:white;\" align=\"center\"><tr><th style=\"visibility:hidden;\">UID</th><th>From</th><th>To</th><th>Connection</th></tr>";		
+					var _tmpUIDs = [];
+					for(q=0; q < response.features.length; q++)
+					{
+						if(response.features[q].attributes.Country_name != res.graphic.attributes.Country_name &&
+						  !_tmpUIDs.includes(response.features[q].attributes.Country_name))
+						{
+							_html +=  "<tr><td style=\"visibility:hidden;\">" + response.features[q].attributes.Country_name + "</td><td>" + res.graphic.attributes.Country_name + "</td><td>" + response.features[q].attributes.Country_name + "</td>";
+							_html += "<td><button type=\"button\" style=\"background-color:#6c757d; border-color:#6c757d;\" class=\"btn btn-dark\" onclick=\"removeEdge(this)\"> Remove </button></td></tr>";
+							 _tmpUIDs.push(response.features[q].attributes.Country_name);
+							
+							var geographicLine = new Polyline();
+							geographicLine.addPath([
+							    [res.graphic.attributes.Longitude, res.graphic.attributes.Latitude],
+							    [response.features[q].attributes.Longitude, response.features[q].attributes.Latitude]
+							  ]);
+							// Create an object for storing attributes related to the line
+							var lineAtt = {
+							  From: res.graphic.attributes.Country_name,
+							  To: response.features[q].attributes.Country_name
+							};
+
+							 var line = geometryEngine.geodesicDensify(geographicLine, 10000);
+							 view.map.findLayerById("connections").add(new Graphic({
+							   geometry: line,
+							   symbol: lineSymbol,
+							   attributes: lineAtt
+							   /*popupTemplate: {
+								title: "Connection Info",
+								actions: [
+								      {
+									title: "Remove from Predictions",
+									id: "removeFromPredictionEdge"
+								      }
+								],
+								content: "" +
+									"<p>From = " + result.graphic.attributes.From_Airport + "</p>" +
+									"<p>" + result.graphic.attributes.From_Name + ", " + result.graphic.attributes.From_Country + "</p>" +
+									"<p>To = " + canvasFlowmapLayer.graphics.items[k].attributes.To_Airport + "</p>" +
+									"<p>" + canvasFlowmapLayer.graphics.items[k].attributes.To_Name + ", " + canvasFlowmapLayer.graphics.items[k].attributes.To_Country + "</p>"
+							    }*/
+							  }));
+						}
+					}
+					_html += "</table>";
+					edgesDiv.innerHTML = _html;
+					view.ui.add(edgesDiv, "top-right");
+				  });				
 			}
-			//break;
-		});
+			break;
+		}		
 	    });
 	});
 });
@@ -266,204 +328,5 @@ function getValues(response)
 	console.log("Results found: " + results.features.length);
     	console.log(results.features);
 }
- 
-view.when(function() {
-    // here we use Papa Parse to load and read the CSV data
-    // we could have also used another library like D3js to do the same
-    Papa.parse('Data/flight_routes_processed_v2_flowmap_reduced.csv', {
-      download: true,
-      header: true,
-      dynamicTyping: true,
-      skipEmptyLines: true,
-      complete: handleCsvParsingComplete
-    });
-  });
-  function handleCsvParsingComplete(results) {
-    var graphicsFromCsvRows = results.data.map(function(datum) {
-      return new Graphic({
-        geometry: {
-          type: 'point',
-          longitude: datum.From_Longitude,
-          latitude: datum.From_Latitude
-        },
-        attributes: datum
-      });
-    });
-    var canvasFlowmapLayer = new CanvasFlowmapLayer({
-      // array of Graphics
-      graphics: graphicsFromCsvRows,
-      // information about the uniqe origin-destinatino fields and geometries
-      originAndDestinationFieldIds: {
-        originUniqueIdField: 'From_Airport_Code',
-        originGeometry: {
-          x: 'From_Longitude',
-          y: 'From_Latitude',
-          spatialReference: {
-            wkid: 4326
-          }
-        },
-        destinationUniqueIdField: 'To_Airport_Code',
-        destinationGeometry: {
-          x: 'To_Longitude',
-          y: 'To_Latitude',
-          spatialReference: {
-            wkid: 4326
-          }
-        }
-      }
-    });
-    canvasFlowmapLayer.title = "Flowmap Connections";
-    view.map.layers.add(canvasFlowmapLayer);
-        
-    // get access to the CanvasFlowmapLayer's layerView to make modifications
-    // of which O-D relationships are flagged for path display
-    view.whenLayerView(canvasFlowmapLayer).then(function(canvasFlowmapLayerView) {
-      // automatically select a few ORIGIN locations for path display
-      // in order to demonstrate the flowmap functionality,
-      // without being overwhelming and showing all O-D relationships
-      // Herat Airport
-      //canvasFlowmapLayerView.selectGraphicsForPathDisplayById('From_Airport_Code', 5539, true, 'SELECTION_NEW');
-      // Tirana International Airport Mother Teresa
-      //canvasFlowmapLayerView.selectGraphicsForPathDisplayById('From_Airport_Code', 13755, true, 'SELECTION_ADD');
-      // establish a hitTest to try to select new O/D relationships
-      // for path display from user interaction;
-      // try either 'pointer-move' or 'click' to see the effects
-      view.on('click', function(event) {
-      // view.on('click', function(event) {
-        var screenPoint = {
-          x: event.x,
-          y: event.y
-        };
-        view.hitTest(screenPoint).then(function(response) {
-          if (!response.results.length) {
-		  var edgesDiv = document.getElementById("edgesDiv");
-	     	  edgesDiv.innerHTML = "";
-		  
-		  // remove previous selection
-		  if(view.map.findLayerById("connections").graphics.length > 0)
-		  {
-		        view.map.findLayerById("connections").graphics.removeAll();
-		  }
-            	  return;
-          }
-          // check if the graphic(s) belongs to the layer of interest
-          // and mark them as selected for Bezier path display
-          response.results.forEach(function(result) {
-            if (result.graphic.layer === canvasFlowmapLayer) {
-              if (result.graphic.isOrigin) {
-                canvasFlowmapLayerView.selectGraphicsForPathDisplayById(
-                  'From_Airport_Code',
-                  result.graphic.attributes.From_Airport_Code,
-                  result.graphic.attributes.isOrigin,
-                  'SELECTION_NEW'
-                );
-              } else {
-                canvasFlowmapLayerView.selectGraphicsForPathDisplayById(
-                  'To_Airport_Code',
-                  result.graphic.attributes.To_Airport_Code,
-                  result.graphic.attributes.isOrigin,
-                  'SELECTION_NEW'
-                );
-	      }
-		
-	      /*var flowCurves: {
-			strokeStyle: 'rgba(255, 0, 51, 0.8)',
-			lineWidth: 0.75,
-			lineCap: 'round',
-			shadowColor: 'rgb(255, 0, 51)',
-			shadowBlur: 1.5
-		      };
-	       */
-		    
-	        //console.log(canvasFlowmapLayer);
-		var edgesDiv = document.getElementById("edgesDiv");
-		var _html = "<table id=\"edge_list_table\" class=\"table table-dark\" style=\"color:white;\" align=\"center\"><tr><th style=\"visibility:hidden;\">UID</th><th>From</th><th>To</th><th>Connection</th></tr>";
-		
-	    	 console.log(canvasFlowmapLayer.graphics.items);
-		 console.log(canvasFlowmapLayer.graphics);
-		 var z=1;
-		 var _tmpUIDs = [];
-		 
-		 // remove previous selection
-		 if(view.map.findLayerById("connections").graphics.length > 0)
-			 view.map.findLayerById("connections").graphics.removeAll();
-		    
-		 for(k=0; k < canvasFlowmapLayer.graphics.items.length; k++)
-		 {
-			 if(!canvasFlowmapLayer.graphics.items[k].attributes.isOrigin && 
-			    result.graphic.attributes.isOrigin && 
-			    (canvasFlowmapLayer.graphics.items[k].attributes.From_Airport_Code == result.graphic.attributes.From_Airport_Code) &&
-			    !_tmpUIDs.includes(canvasFlowmapLayer.graphics.items[k].uid))
-			 {
-				 console.log("inside loop... " + k);
-				_html +=  "<tr><td style=\"visibility:hidden;\">" + canvasFlowmapLayer.graphics.items[k].uid + "</td><td>" + canvasFlowmapLayer.graphics.items[k].attributes.From_Airport_Code + "</td><td>" + canvasFlowmapLayer.graphics.items[k].attributes.To_Airport_Code + "</td>";
-				_html += "<td><button type=\"button\" style=\"background-color:#6c757d; border-color:#6c757d;\" class=\"btn btn-dark\" onclick=\"removeEdge(this)\"> Remove </button></td></tr>";
-				 
-				 _tmpUIDs.push(canvasFlowmapLayer.graphics.items[k].uid);
-				var geographicLine = new Polyline();
-				geographicLine.addPath([
-				    [result.graphic.attributes.From_Longitude, result.graphic.attributes.From_Latitude],
-				    [canvasFlowmapLayer.graphics.items[k].attributes.To_Longitude, canvasFlowmapLayer.graphics.items[k].attributes.To_Latitude]
-				  ]);
 
-				// Create a symbol for drawing the line
-				var lineSymbol = {
-				  type: "simple-line", // autocasts as SimpleLineSymbol()
-				  color: [255,0,0,0.5],
-				  width: 0.75,
-				  cap : "round"
-				};
-
-				// Create an object for storing attributes related to the line
-				var lineAtt = {
-				  id: canvasFlowmapLayer.graphics.items[k].uid
-				};
-
-				 var line = geometryEngine.geodesicDensify(geographicLine, 10000);
-				 view.map.findLayerById("connections").add(new Graphic({
-				   geometry: line,
-				   symbol: lineSymbol,
-				   attributes: lineAtt,
-				   /*popupTemplate: {
-				   	title: "Connection Info",
-					actions: [
-					      {
-						title: "Remove from Predictions",
-						id: "removeFromPredictionEdge"
-					      }
-				        ],
-   					content: "" +
-						"<p>From = " + result.graphic.attributes.From_Airport + "</p>" +
-						"<p>" + result.graphic.attributes.From_Name + ", " + result.graphic.attributes.From_Country + "</p>" +
-						"<p>To = " + canvasFlowmapLayer.graphics.items[k].attributes.To_Airport + "</p>" +
-						"<p>" + canvasFlowmapLayer.graphics.items[k].attributes.To_Name + ", " + canvasFlowmapLayer.graphics.items[k].attributes.To_Country + "</p>"
-				    }*/
-				  }));
-				 
-				 z++;
-			 }
-		 }		  
-		 _html += "</table>";
-	    	  edgesDiv.innerHTML = _html;
-		  view.ui.add(edgesDiv, "top-right");
-	    	  //document.getElementByID("edge-list-uid").style.visibility = "hidden";
-	        
-	      }
-	      else{
-		     var edgesDiv = document.getElementById("edgesDiv");
-		     edgesDiv.innerHTML = "";
-		      
-		      // remove previous selection
-		      if (result.graphic.layer !== view.map.findLayerById("connections")) {
-			      if(view.map.findLayerById("connections").graphics.length > 0)
-			      {
-				      view.map.findLayerById("connections").graphics.removeAll();
-			      }
-		      }
-	      }
-          });
-        });
-      });
-    });
-  } 
 });
